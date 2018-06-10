1a01ee6e2258e16c06d82fc6fb6192d19b74ca31
Jean-Baptiste Clouard <jeanbaptiste.clouard@gmail.com>
2018-06-10 13:03:29 +0200

    adding group selection

--- roxaneApp/imports/client/ui/questions/Questions.js
+++ roxaneApp/imports/client/ui/questions/Questions.js
@@ -8,10 +8,15 @@ export default class Questions extends Component {
   constructor(){
     super();
     this.state={
+      group : "",
       gender : "",
       music : "",
       sport: ""
     }
+    this.onChangeGroupAlone = this.onChangeGroupAlone.bind(this);
+    this.onChangeGroupCouple = this.onChangeGroupCouple.bind(this);
+    this.onChangeGroupFamily = this.onChangeGroupFamily.bind(this);
+    this.onChangeGroupFriends = this.onChangeGroupFriends.bind(this);
     this.onChangeGenderMale = this.onChangeGenderMale.bind(this);
     this.onChangeGenderFemale = this.onChangeGenderFemale.bind(this);
     this.onChangeMusicRock = this.onChangeMusicRock.bind(this);
@@ -24,6 +29,34 @@ export default class Questions extends Component {
     this.onChangeSport3Period = this.onChangeSport3Period.bind(this);
     this.onChangeSportCycling = this.onChangeSportCycling.bind(this);
     this.onChangeSportFootball = this.onChangeSportFootball.bind(this);
+  }
+
+  onChangeGroupAlone(e){
+    e.preventDefault();
+    this.setState({
+      group: "alone"
+    });
+  }
+
+  onChangeGroupCouple(e){
+    e.preventDefault();
+    this.setState({
+      group: "couple"
+    });
+  }
+
+  onChangeGroupFamily(e){
+    e.preventDefault();
+    this.setState({
+      group: "family"
+    });
+  }
+
+  onChangeGroupFriends(e){
+    e.preventDefault();
+    this.setState({
+      group: "friends"
+    });
   }
 
   onChangeGenderMale(e){
@@ -113,12 +146,18 @@ export default class Questions extends Component {
   submitQuestions(e){
     e.preventDefault();
     const birthdate = ReactDOM.findDOMNode(this.refs.birthDate).value.trim();
-    console.log(birthdate, this.state.gender, this.state.music, this.state.sport)
+    const depdate = ReactDOM.findDOMNode(this.refs.depDate).value.trim();
+    const retdate = ReactDOM.findDOMNode(this.refs.retDate).value.trim();
+    console.log(this.state.group, birthdate, this.state.gender, this.state.music, this.state.sport)
     //window.location = '/circuits';
   }
 
   render(){
 
+    let selectedAlone = (this.state.group=="alone") ? "selectedGroup" : "notSelectedGroup";
+    let selectedCouple = (this.state.group=="couple") ? "selectedGroup" : "notSelectedGroup";
+    let selectedFamily = (this.state.group=="family") ? "selectedGroup" : "notSelectedGroup";
+    let selectedFriends = (this.state.group=="friends") ? "selectedGroup" : "notSelectedGroup";
     let selectedMale = (this.state.gender=="male") ? "selectedGender" : "notSelectedGender";
     let selectedFemale = (this.state.gender=="female") ? "selectedGender" : "notSelectedGender";
     let selectedRock = (this.state.music=="rock") ? "selectedMusic" : "notSelectedMusic";
@@ -139,6 +178,51 @@ export default class Questions extends Component {
           <TextQuestions />
           <form className="questions-form" onSubmit={this.submitQuestions.bind(this)}>
             <center>
+              <input
+                type="text"
+                ref="goingTo"
+                className="form-control questions-text"
+                placeholder="Where do you want to go?"
+                aria-label="GoingTo"
+                aria-describedby="basic-addon2"
+              /><br/>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text questions-text" id="basic-addon3">Departure date:</span>
+                </div>
+                <input
+                  type="date"
+                  ref="depDate"
+                  className="form-control questions-text"
+                  placeholder="Departure Date"
+                  aria-label="Departure Date"
+                  aria-describedby="basic-addon3"
+                />
+              </div>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text questions-text" id="basic-addon4">Return date:</span>
+                </div>
+                <input
+                  type="date"
+                  ref="retDate"
+                  className="form-control questions-text"
+                  placeholder="Return Date"
+                  aria-label="Return Date"
+                  aria-describedby="basic-addon4"
+                />
+              </div><br/><br/>
+              <h5 className="questions-text">You will travel:</h5><br/>
+              <span>
+                  <label className="questions-text">Alone</label>
+                  <label className="questions-text">In couple</label>
+                  <label className="questions-text">With the family</label>
+                  <label className="questions-text">With friends</label><br/>
+                  <label className={selectedAlone} onClick={this.onChangeGroupAlone}><img className="musicIcon" src="/Questions/002-boy.png"/></label>
+                  <label className={selectedCouple} onClick={this.onChangeGroupCouple}><img className="musicIcon" src="/Questions/003-parents.png"/></label>
+                  <label className={selectedFamily} onClick={this.onChangeGroupFamily}><img className="musicIcon" src="/Questions/001-family.png"/></label>
+                  <label className={selectedFriends} onClick={this.onChangeGroupFriends}><img className="musicIcon" src="/Questions/004-friendship.png"/></label>
+              </span><br/><br/>
               <div className="input-group mb-3">
                 <div className="input-group-prepend">
                   <span className="input-group-text questions-text" id="basic-addon1">Birth date:</span>
@@ -158,23 +242,31 @@ export default class Questions extends Component {
                   <label className={selectedFemale} onClick={this.onChangeGenderFemale}><FontAwesome className="male-female" name='female' size='5x' /></label>
               </span><br/><br/>
               <h5 className="questions-text">What about music?</h5><br/>
-              <span className="male-female">
+              <span>
+                  <label className="questions-text">Rock</label>
+                  <label className="questions-text">Classical</label>
+                  <label className="questions-text">Rap</label>
+                  <label className="questions-text">Jazz</label><br/>
                   <label className={selectedRock} onClick={this.onChangeMusicRock}><img className="musicIcon" src="/Questions/004-guitar.png"/></label>
                   <label className={selectedClassical} onClick={this.onChangeMusicClassical}><img className="musicIcon" src="/Questions/002-viola.png"/></label>
                   <label className={selectedRap} onClick={this.onChangeMusicRap}><img className="musicIcon" src="/Questions/003-rap.png"/></label>
                   <label className={selectedJazz} onClick={this.onChangeMusicJazz}><img className="musicIcon" src="/Questions/001-trumpet.png"/></label>
               </span><br/><br/>
               <h5 className="questions-text">And sports?</h5><br/>
-              <span className="male-female">
+              <span>
+                  <label className="questions-text">Football</label>
+                  <label className="questions-text">Yoga</label>
+                  <label className="questions-text">Cycling</label>
+                  <label className="questions-text">Rugby</label><br/>
                   <label className={selectedFootball} onClick={this.onChangeSportFootball}><img className="musicIcon" src="/Questions/006-soccer.png"/></label>
                   <label className={selectedYoga} onClick={this.onChangeSportYoga}><img className="musicIcon" src="/Questions/001-yoga.png"/></label>
                   <label className={selectedCycling} onClick={this.onChangeSportCycling}><img className="musicIcon" src="/Questions/005-bike.png"/></label>
                   <label className={selectedRugby} onClick={this.onChangeSportRugby}><img className="musicIcon" src="/Questions/003-rugby.png"/></label>
+                  <label className="questions-text">Tennis</label>
+                  <label className="questions-text">3rd Period</label><br/>
                   <label className={selectedTennis} onClick={this.onChangeSportTennis}><img className="musicIcon" src="/Questions/004-tennis.png"/></label>
                   <label className={selected3Period} onClick={this.onChangeSport3Period}><img className="musicIcon" src="/Questions/002-beer.png"/></label>
               </span><br/><br/>
-              <PersonalQuestions />
-              <TripQuestions />
               <button type='submit' className="button-next">Next</button>
             </center>
           </form>
@@ -200,25 +292,6 @@ class PersonalQuestions extends Component{
     return(
       <div className="pers-questions-form">
         <div className="input-group center">
-          What is your favorite sport among :
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <input type="radio" aria-label="foot" id="inlineRadio21" value="foot" name="gridRadios2"/>
-              <label className="form-check-label" for="inlineRadio21">Football</label><br/>
-              <input type="radio" aria-label="yoga" id="inlineRadio22" value="yoga" name="gridRadios2"/>
-              <label className="form-check-label" for="inlineRadio22">Yoga</label><br/>
-              <input type="radio" aria-label="cycling" id="inlineRadio23" value="cycling" name="gridRadios2"/>
-              <label className="form-check-label" for="inlineRadio23">Walking or Cycling</label><br/>
-              <input type="radio" aria-label="rugby" id="inlineRadio24" value="rugby" name="gridRadios2"/>
-              <label className="form-check-label" for="inlineRadio24">Rugby</label><br/>
-              <input type="radio" aria-label="tennis" id="inlineRadio25" value="tennis" name="gridRadios2"/>
-              <label className="form-check-label" for="inlineRadio25">Tennis</label><br/>
-              <input type="radio" aria-label="board_games" id="inlineRadio26" value="board_games" name="gridRadios2"/>
-              <label className="form-check-label" for="inlineRadio26">Board games and books</label><br/>
-            </div>
-          </div>
-        </div>
-        <div className="input-group center">
           Do you feel more like ?
           <div className="input-group-prepend">
             <div className="input-group-text">
@@ -232,49 +305,6 @@ class PersonalQuestions extends Component{
               <label className="form-check-label" for="inlineRadio34">Cindarella</label><br/>
               <input type="radio" aria-label="snow" id="inlineRadio35" value="snow" name="gridRadios3"/>
               <label className="form-check-label" for="inlineRadio35">Jon Snow</label><br/>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
-
-class TripQuestions extends Component{
-  render(){
-    return(
-      <div className="trip-questions-form">
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon2">Where do you want to go ?</span>
-          </div>
-          <input type="text" className="form-control" placeholder="GoingTo" aria-label="GoingTo" aria-describedby="basic-addon2" />
-          <input type="checkbox" id="surprise"/>I am also up for surprises ...
-        </div>
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon3">Departure date:</span>
-          </div>
-          <input type="date" className="form-control" placeholder="Departure Date" aria-label="Departure Date" aria-describedby="basic-addon3" />
-        </div>
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon4">Return date:</span>
-          </div>
-          <input type="date" className="form-control" placeholder="Return Date" aria-label="Return Date" aria-describedby="basic-addon4" />
-        </div>
-        <div className="input-group center">
-          You travel:
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <input type="radio" aria-label="Alone" id="inlineRadio41" value="alone" name="gridRadios4"/>
-              <label className="form-check-label" for="inlineRadio41">Alone</label><br/>
-              <input type="radio" aria-label="Couple" id="inlineRadio42" value="couple" name="gridRadios4"/>
-              <label className="form-check-label" for="inlineRadio42">In a couple</label><br/>
-              <input type="radio" aria-label="Family" id="inlineRadio43" value="family" name="gridRadios4"/>
-              <label className="form-check-label" for="inlineRadio43">With entire family</label><br/>
-              <input type="radio" aria-label="Friends" id="inlineRadio44" value="friends" name="gridRadios4"/>
-              <label className="form-check-label" for="inlineRadio44">With friends</label><br/>
             </div>
           </div>
         </div>
